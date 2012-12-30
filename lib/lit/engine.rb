5d87054af7997942c8c245ac038a91da0dd5abd2
Maciej Litwiniuk <maciej@litwiniuk.net>
2012-12-30 13:29:31 +0100

    precompile assets

--- lib/lit/engine.rb
+++ lib/lit/engine.rb
@@ -3,8 +3,9 @@ module Lit
   class Engine < ::Rails::Engine
     isolate_namespace Lit
 
+
     initializer "lit.assets.precompile" do |app|
-      app.config.assets.precompile = %w(application.css application.js)
+      Rails.application.config.assets.precompile += %w(lit/application.css lit/application.js)
     end
   end
 end
