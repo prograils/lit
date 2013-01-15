ca71d1e66e0f9e4f012667b65731e88dd7f6a313
Maciej Litwiniuk <maciej@litwiniuk.net>
2013-01-15 13:38:22 +0100

    prefixing redis storage keys

--- lib/lit/adapters/redis_storage.rb
+++ lib/lit/adapters/redis_storage.rb
@@ -12,24 +12,33 @@ module Lit
     end
 
     def [](key)
-      Lit.redis.get(key)
+      Lit.redis.get(_prefixed_key(key))
     end
 
     def []=(k, v)
-      Lit.redis.set(k.to_s, v.to_s)
+      Lit.redis.set(_prefixed_key(k).to_s, v.to_s)
     end
 
     def clear
-      Lit.redis.flushall
+      Lit.redis.del self.keys
       save
     end
 
     def keys
-      Lit.redis.keys
+      Lit.redis.keys(_prefixed_key+"*")
     end
 
     def has_key?(key)
-      Lit.redis.exists(key)
+      Lit.redis.exists(_prefixed_key(key))
     end
+
+    private
+      def _prefixed_key(key="")
+        prefix = "lit:"
+        if Lit.storage_options.is_a?(Hash)
+          prefix += "#{Lit.storage_options[:prefix]}:" if Lit.storage_options.has_key?(:prefix)
+        end
+        prefix+key
+      end
   end
 end
