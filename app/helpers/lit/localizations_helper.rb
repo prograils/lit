f87880aa301642eec9f06c6a6ed51476faa0c98b
Ivan <ivan.eng.controle@gmail.com>
2015-01-19 14:39:13 -0200

    Added a url parameter to specify a locale to list its localization keys.

--- app/helpers/lit/localizations_helper.rb
+++ app/helpers/lit/localizations_helper.rb
@@ -11,5 +11,10 @@ module Lit
     def allow_wysiwyg_editor?(key)
       Lit.all_translations_are_html_safe || key.to_s =~ /(\b|_|\.)html$/
     end
+
+    def locales
+      return [@current_locale] if @current_locale
+      return I18n.available_locales if !@current_locale
+    end
   end
 end
