c9d4590f50e64a07206bc9b5b41fc7d5abc5800c
Ivan <ivan.eng.controle@gmail.com>
2015-01-21 12:10:05 -0200

    Fixed dummy test app locale config.

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -27,6 +27,8 @@ module Dummy
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
+    config.i18n.available_locales = %w(en pl)
     config.i18n.default_locale = :en
 
     # Configure the default encoding used in templates for Ruby 1.9.
