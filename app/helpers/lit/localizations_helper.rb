f22f1bef2905267b833e5e3b5cd120123d96c79a
Ivan <ivan.eng.controle@gmail.com>
2015-01-21 12:10:53 -0200

    Extracted view code into a helper.

--- app/helpers/lit/localizations_helper.rb
+++ app/helpers/lit/localizations_helper.rb
@@ -16,5 +16,13 @@ module Lit
       return I18n.available_locales if !@current_locale || @current_locale == ''
       return [@current_locale] if @current_locale
     end
+
+    def default_localization(localization_key, locale=:en)
+      if lk = localization_key.localizations.for_locale(locale).first
+        lk.default_value
+      else
+        'null'
+      end
+    end
   end
 end
