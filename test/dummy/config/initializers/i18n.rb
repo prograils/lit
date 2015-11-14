5a0796804eb1a15c40ed6cb3f0310e4ffea6ce5b
Maciej Litwiniuk <maciej@litwiniuk.net>
2015-11-14 20:33:18 +0100

    Update exception handler for dummy app
    `t` helper behaves now in the same way in Rails 3, 4 and 4.1

--- test/dummy/config/initializers/i18n.rb
+++ test/dummy/config/initializers/i18n.rb
@@ -4,12 +4,22 @@ if ::Rails::VERSION::MAJOR == 3 ||
 
   # Override exception handler to more carefully html-escape missing-key results.
   class HtmlSafeI18nExceptionHandler
+    Missing = I18n.const_defined?(:MissingTranslation) ? I18n::MissingTranslation : I18n::MissingTranslationData
+
+    def initialize(original_exception_handler)
+      @original_exception_handler = original_exception_handler
+    end
+
     def call(exception, locale, key, options)
-      keys = exception.keys.map { |k| Rack::Utils.escape_html k }
-      key = keys.last.to_s.gsub('_', ' ').gsub(/\b('?[a-z])/) { $1.capitalize }
-      %(<span class="translation_missing" title="translation missing: #{keys.join('.')}">#{key}</span>)
+      if exception.is_a?(Missing) && options[:rescue_format] == :html
+        keys = exception.keys.map { |k| Rack::Utils.escape_html k }
+        key = keys.last.to_s.gsub('_', ' ').gsub(/\b('?[a-z])/) { $1.capitalize }
+        %(<span class="translation_missing" title="translation missing: #{keys.join('.')}">#{key}</span>)
+      else
+        @original_exception_handler.call(exception, locale, key, options)
+      end
     end
   end
 
-  I18n.exception_handler = HtmlSafeI18nExceptionHandler.new
+  I18n.exception_handler = HtmlSafeI18nExceptionHandler.new(I18n.exception_handler)
 end
