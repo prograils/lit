990fe232fb99aa21d45a49bf93c0e236469a1d7d
Maciej Litwiniuk <maciej@litwiniuk.net>
2017-05-16 12:37:19 +0200

    Remove comment stating, that Lit::FrontendHelper gets loaded

--- lib/lit/engine.rb
+++ lib/lit/engine.rb
@@ -2,12 +2,6 @@ module Lit
   class Engine < ::Rails::Engine
     isolate_namespace Lit
 
-    config.after_initialize do
-      ActiveSupport.on_load :action_controller do
-        puts 'loading Lit::FrontendHelper'.upcase
-        #ActionController::Base.send(:helper, Lit::FrontendHelper)
-      end
-    end
     initializer 'lit.assets.precompile' do |app|
       app.config.assets.precompile += %w(lit/application.css lit/application.js)
       app.config.assets.precompile += %w(lit/lit_frontend.css lit/lit_frontend.js)
