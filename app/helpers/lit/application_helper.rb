97e931a47451b6a3cbe3800542b7ee165f3cabae
zhdanov <zhdanov09zh@gmail.com>
2018-01-27 17:42:37 +0300

    fix helpers, and js remote data edit for i18n fields

--- app/helpers/lit/application_helper.rb
+++ app/helpers/lit/application_helper.rb
@@ -1,4 +1,7 @@
 module Lit
   module ApplicationHelper
+    def draw_icon(icon, opts = {})
+      raw("<i class=\"fa fa-#{icon} #{opts[:class]}\" title=\"#{opts[:title]}\" ></i>")
+    end
   end
 end
