22eda3a7472cfa53ad02fd7d2040c4c7561fb107
Zhdanov <gragor20@gmail.com>
2018-01-27 15:33:47 +0300

    Update 20180101010109_lit_add_sync_complete_to_lit_sources.rb

--- db/migrate/20180101010109_lit_add_sync_complete_to_lit_sources.rb
+++ db/migrate/20180101010109_lit_add_sync_complete_to_lit_sources.rb
@@ -1,4 +1,4 @@
-class LitAddSyncCompleteToLitSources < ActiveRecord::Migration
+class LitAddSyncCompleteToLitSources < ActiveRecord::Migration[5.1]
   def up
     return if column_exists?(:lit_sources, :sync_complete)
     add_column :lit_sources, :sync_complete, :boolean
