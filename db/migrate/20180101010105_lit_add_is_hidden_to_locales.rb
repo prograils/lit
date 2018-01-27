9d06d92c6bb79333723ed88134da4c6789a08642
Zhdanov <gragor20@gmail.com>
2018-01-27 15:32:51 +0300

    Update 20180101010105_lit_add_is_hidden_to_locales.rb

--- db/migrate/20180101010105_lit_add_is_hidden_to_locales.rb
+++ db/migrate/20180101010105_lit_add_is_hidden_to_locales.rb
@@ -1,4 +1,4 @@
-class LitAddIsHiddenToLocales < ActiveRecord::Migration
+class LitAddIsHiddenToLocales < ActiveRecord::Migration[5.1]
   def up
     return if column_exists?(:lit_locales, :is_hidden)
     add_column :lit_locales, :is_hidden, :boolean, default: false
