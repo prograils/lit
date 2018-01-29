fcec0fb4682f07a0b1660f73c6753720b95f32cf
Maciej Litwiniuk <maciej@litwiniuk.net>
2018-01-29 16:51:04 +0100

    Ensure language flags are precompiled and lit is able to use jquery

--- lib/lit/engine.rb
+++ lib/lit/engine.rb
@@ -1,5 +1,7 @@
 module Lit
   class Engine < ::Rails::Engine
+    require 'jquery-rails'
+
     config.autoload_paths += %W[#{Lit::Engine.root}/app/controllers/lit/concerns]
 
     isolate_namespace Lit
@@ -7,6 +9,12 @@ module Lit
     initializer 'lit.assets.precompile' do |app|
       app.config.assets.precompile += %w[lit/application.css lit/application.js]
       app.config.assets.precompile += %w[lit/lit_frontend.css lit/lit_frontend.js]
+      # add language flags to list of precompiled assets
+      if app.config.i18n.available_locales
+        app.config.i18n.available_locales.each do |l|
+          app.config.assets.precompile << "lit/famfamfam_flags/#{l.to_s[0,2]}.png"
+        end
+      end
     end
 
     initializer 'lit.migrations.append' do |app|
