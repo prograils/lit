e88950842e0cd820b4dd61daa9a0f423a7320089
Philippe Vaucher <philippe.vaucher@stvs.com>
2018-07-10 11:11:13 +0200

    Improve error handling

--- app/assets/javascripts/lit/lit_frontend.js
+++ app/assets/javascripts/lit/lit_frontend.js
@@ -9,9 +9,12 @@
   buildLocalizationForm = function(e){
     var $this = $(this);
     var meta = $('meta[name="lit-url-base"]');
-    if(meta.length > 0){
+
+    if(meta.length > 0)
       getLocalizationPath($this, meta);
-    }
+    else
+      console.error('cannot find lit base url');
+
     e.stopPropagation();
     return false;
   };
@@ -59,8 +62,8 @@
         console.log('saved ' + elem.data('key'));
       },
       error: function(){
-        console.log('problem saving ' + elem.data('key'));
-        alert('ups, ops, something went wrong');
+        console.error('cannot save ' + elem.data('key'));
+        alert('cannot save ' + elem.data('key'));
       }
     });
   };
