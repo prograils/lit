574b0912d8f0ad7804a1e236bd8db252176601e7
Maciej Litwiniuk <maciej@litwiniuk.net>
2018-11-12 17:06:16 +0100

    Introduce a list of keys to ignore for auto-humanize

--- test/unit/i18n_backend_test.rb
+++ test/unit/i18n_backend_test.rb
@@ -7,12 +7,17 @@ class I18nBackendTest < ActiveSupport::TestCase
 
   def setup
     @old_backend = I18n.backend
+    @old_locale = I18n.locale
+    @old_humanize_key = Lit.humanize_key
     @old_available_locales = ::Rails.configuration.i18n.available_locales
   end
 
   def teardown
     ::Rails.configuration.i18n.available_locales = @old_available_locales
     I18n.backend = @old_backend
+    I18n.backend = @old_backend
+    I18n.locale = @old_locale
+    Lit.humanize_key = @old_humanize_key
   end
 
   test 'properly returns available locales' do
@@ -24,5 +29,28 @@ class I18nBackendTest < ActiveSupport::TestCase
     ::Rails.configuration.i18n.available_locales = [:en]
     I18n.backend = Backend.new(Lit.loader.cache)
     assert_equal 1, I18n.backend.available_locales.count
+  end
+
+  test 'auto-humanizes key when Lit.humanize_key=true' do
+    Lit.humanize_key = true
+    I18n.locale = :en
+    test_key = 'this_will_get_humanized'
+    humanized_key = 'This will get humanized'
+    assert_equal I18n.t(test_key), humanized_key
+    lk = Lit::LocalizationKey.find_by localization_key: test_key
+    locale = Lit::Locale.find_by locale: 'en'
+    l = lk.localizations.where(locale: locale).first
+    assert_equal l.default_value, humanized_key
+  end
+
+  test 'wont humanize key, if key is ignored' do
+    Lit.humanize_key = true
+    I18n.locale = :en
+    test_key = 'date.this_will_get_humanized'
+    assert_equal I18n.t(test_key), 'translation missing: en.date.this_will_get_humanized'
+    lk = Lit::LocalizationKey.find_by localization_key: test_key
+    locale = Lit::Locale.find_by locale: 'en'
+    l = lk.localizations.where(locale: locale).first
+    assert_nil l.default_value
   end
 end
