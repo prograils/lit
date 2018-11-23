05b22407eddd1b59ecac30ffe296086df5aab359
Maciej Litwiniuk <maciej@litwiniuk.net>
2018-11-23 12:41:12 +0100

    Remove pasted text from test/dummy/db/schema.rb

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -1,4 +1,4 @@
-#, on: :update This file is auto-generated from the current state of the database. Instead
+# This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
