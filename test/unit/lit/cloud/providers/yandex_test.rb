5febbf32192a2c45fa9dbaf146567f59bdeb8cc1
Michał Buszkiewicz <embed1@gmail.com>
2018-12-12 12:53:41 +0100

    Make Yandex provider test independent from env

--- test/unit/lit/cloud/providers/yandex_test.rb
+++ test/unit/lit/cloud/providers/yandex_test.rb
@@ -2,11 +2,20 @@
 
 require 'test_helper'
 require 'lit/cloud/providers/yandex'
+require 'minitest/mock'
 
-describe Lit::Cloud::Providers::Yandex, :vcr do
+describe Lit::Cloud::Providers::Yandex,
+         vcr: {
+           match_requests_on: [:method,
+                               VCR.request_matchers.uri_without_param(:key)]
+         } do
   let(:text) { 'The quick brown fox jumps over the lazy dog.' }
   let(:from) { 'en' }
   let(:to) { 'pl' }
+
+  before do
+    ENV.stubs(:[]).with('YANDEX_TRANSLATE_API_KEY').returns('fakekey')
+  end
 
   describe 'when only :to language is given' do
     subject do
@@ -20,7 +29,7 @@ describe Lit::Cloud::Providers::Yandex, :vcr do
     end
 
     describe 'when array of strings is given' do
-      let(:text) { ['awesome', 'stuff'] }
+      let(:text) { %w[awesome stuff] }
 
       it 'translates array of strings to target language' do
         subject.length.must_equal 2
@@ -40,7 +49,7 @@ describe Lit::Cloud::Providers::Yandex, :vcr do
     end
 
     describe 'when array of strings is given' do
-      let(:text) { ['awesome', 'stuff'] }
+      let(:text) { %w[awesome stuff] }
 
       it 'translates array of strings to target language' do
         subject.length.must_equal 2
